b774c7f303e6f37f180284c15a378d708bc011d6
Mikael Kindborg <mikael.kindborg@gmail.com>
2013-04-09 08:48:02 +0200

    Added documentation that is to be under version control, added .gitignore, updated analyse.rb

--- analyse.rb
+++ analyse.rb
@@ -1,28 +1,63 @@
 require 'fileutils'
 require 'pathname'
 
-def countAndCompareFiles
-  files1 = Pathname.glob("./mosync-doc-exports/**/*.html").collect { |item| 
-    item.to_s.split("./mosync-doc-exports/")[1] } 
-  files2 = Pathname.glob("./mosync-doc-exports-2/**/*.html").collect { |item| 
-    item.to_s.split("./mosync-doc-exports-2/")[1] } 
-  numFiles1 = files1.length
-  numFiles2 = files2.length
-  puts "Files in 1: " + numFiles1.to_s
-  puts "Files in 2: " + numFiles2.to_s
-  puts "New files:"
+
+def exportsDir1
+ "./mosync-doc-exports/"
+end
+
+def exportsDir2
+ "./mosync-doc-exports-2/"
+end
+
+def allHtmlFiles(path)
+  Pathname.glob(path + "**/*.html").collect { |item| 
+    item.to_s.split(path)[1] }
+end
+
+def countFiles
+  numFiles1 = allHtmlFiles(exportsDir1()).length
+  numFiles2 = allHtmlFiles(exportsDir2()).length
+  puts "========================================================="
+  puts "HTML files in " + exportsDir1() + ": " + numFiles1.to_s
+  puts "HTML files in " + exportsDir2() + ": " + numFiles2.to_s
+  puts "Number of new files: " + (numFiles2 - numFiles1).to_s
+  puts "========================================================="
+end
+
+def newFiles
+  files1 = allHtmlFiles(exportsDir1())
+  files2 = allHtmlFiles(exportsDir2())
+  puts "========================================================="
+  puts "New files in " + exportsDir1()
   puts files2 - files1
-  puts "Old files:"
-  puts files1
+  puts "========================================================="
 end
 
 def listAllFiles
-  files = Pathname.glob("./mosync-doc-exports/**/*.html").collect { |item| 
-    item.to_s.split("./mosync-doc-exports/")[1] } 
-  puts "============================================================"
-  puts "All files (export 1):"
-  puts files.sort
+  dir = exportsDir2()
+  puts "========================================================="
+  puts "All files in " + dir
+  puts allHtmlFiles(dir).sort
+  puts "========================================================="
 end
 
-#countAndCompareFiles
-listAllFiles
+if (ARGV.include? "new")
+  newFiles
+elsif (ARGV.include? "count")
+  countFiles
+elsif (ARGV.include? "all")
+  listAllFiles
+elsif (ARGV.include? "links")
+  listAllLinks
+else
+  puts "========================================================="
+  puts "Options:"
+  puts "  all (show all files in new export)"
+  puts "  new (show new files in new export)"
+  puts "  count (count files in all exports)"
+  puts "  links (show all links in new export)"
+  puts "(please manually update the script to specify"
+  puts "export directories)"
+  puts "========================================================="
+end
